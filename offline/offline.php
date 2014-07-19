44c91af35a5b73688c8a9a293a43065f49092b58
Matthijs Alles <admin@bixie.nl>
2014-07-19 12:52:18 +0200

    edit user after order

--- offline/offline.php
+++ offline/offline.php
@@ -14,8 +14,46 @@ class plgZoocart_PaymentOffline extends JPaymentDriver {
 	public function render($data = array()) {
 		$app = App::getInstance('zoo');
 		$data['order']->state = $app->zoocart->getConfig()->get('payment_pending_orderstate', 4);
+		$address = $data['order']->getBillingAddress();
+		$elements = $address->getElements();
+		$names = array();
+		$email = '';
+		foreach($elements as $key => $element) {
+			$key = $element->config->get('billing', '');
+			$value = $element->get('value');
+			if ($key == 'name') {
+				$names[] = $value;
+			}
+			if ($key == 'personal_id') {
+				$email = $element->get('value');
+			}
+		}
+		
+		$name = ($names[3]?$names[3]:$names[2]).' '.$names[4];
+		$user = JFactory::getUser();
+		if ($user->email != $email) {
+			$success = false;
+			//bestaat er een user met die mail?
+			$db = JFactory::getDbo();
+			$db->setQuery("SELECT * FROM #__users WHERE email = '$email' OR username = '$email'");
+			$existing = $db->loadObject();
+			if ($existing->id) {
+				//wijzig order (kan niet, gebruiker heeft dan geen rechten meer om order te zien)
+				// $data['order']->user_id = $existing->id;
+				$user->set('name',$name);
+			} else {
+				//wijzig user
+				$user->set('username',$email);
+				$user->set('email',$email);
+				$user->set('name',$name);
+			}
+			$success = $user->save();
+			if(!$success) {
+				$errors = $user->getErrors();
+				echo implode($errors);
+			}
+		}
 		$app->zoocart->table->orders->save($data['order']);
-
 		return parent::render($data);
 	}
 }
